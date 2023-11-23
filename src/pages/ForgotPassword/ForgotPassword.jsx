53afc5259516e141ed9a6c5bea71e78eba0259f5
abelgh91 <abelgh91@gmail.com>
2023-11-23 16:07:02 +0100

    forgotpassword

--- src/pages/ForgotPassword/ForgotPassword.jsx
+++ src/pages/ForgotPassword/ForgotPassword.jsx
@@ -1,7 +1,70 @@
+import { useForm } from "react-hook-form";
 import "./ForgotPassword.css";
+import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { forgotPasswordUser } from "../../services/user.service";
+import { useForgotPasswordError } from "../../hooks";
 
 export const ForgotPassword = () => {
+  const {handleSubmit, register} = useForm();
+  const [res, setRes] = useState({});
+  const [send, setSend] = useState(false);
+  const [forgotOk, setForgotOk] = useState(false);
+
+// gestionamos los datos del formulario
+
+  const formSubmit = async (formData) => {
+    setSend(true);
+    setRes(await forgotPasswordUser(formData));
+    setSend(false);
+  };
+
+// useeffect que gestiona errores y 200
+useEffect(() => {
+  useForgotPasswordError(res, setRes, setForgotOk); // parametros en orden y con el mismo nombre
+}, [res]);
+
+//estado de navegacion (ok)
+if (forgotOk) {
+  return <Navigate to="/login" />;
+}
+
   return (
-    <div>ForgotPassword</div>
+    <>
+      <div className="form-wrap">
+        <h1>Change your password 💱</h1>
+
+        <form onSubmit={handleSubmit(formSubmit)}>
+          <div className="user_container form-group">
+            <input
+              className="input_user"
+              type="text"
+              id="email"
+              name="email"
+              autoComplete="false"
+              {...register("email", { required: true })}
+            />
+            <label htmlFor="custom-input" className="custom-placeholder">
+              Email
+            </label>
+          </div>
+
+          <div className="btn_container">
+            <button
+              className="btn"
+              type="submit"
+              disabled={send}
+              style={{ background: send ? "#49c1a388" : "#49c1a2" }}
+            >
+              Change password
+            </button>
+          </div>
+
+          <p className="bottom-text">
+            <small>Enter your email to send you the new password 💌</small>
+          </p>
+        </form>
+      </div>
+    </>
   )
 };
